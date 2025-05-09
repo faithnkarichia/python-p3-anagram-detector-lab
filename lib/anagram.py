b47378c8699cd91e69e212319781f8c32511088e
Faith <kashfaith77@gmail.com>
2025-05-09 14:59:22 +0300

    Anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+
+    def match(self,list):
+        sorted_word=sorted(self.word)
+        results=[]
+        for _word in list:
+            if sorted(_word.lower())==sorted_word and _word !=self.word:
+                results.append(_word)
+
+        return results
+
